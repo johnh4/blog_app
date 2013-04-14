0f1966c1cc703591fb024cd1988834a59654f572
John Howlett <jehowl4@gmail.com>
2013-04-14 19:37:13 -0400

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 BlogApp::Application.routes.draw do
+  get "users/new"
+
+  match '/signup', to: "users#new"
+
   get "static_pages/home"
   get "static_pages/about"
 
   get "static_pages/help"
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
