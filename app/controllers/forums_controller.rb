0591b55564f196a6b48025e6e2c9b0e65cf23c2b
John Howlett <jehowl4@gmail.com>
2013-05-18 11:12:37 -0400

    Add in-app Forum creation, improve Forms

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -1,8 +1,19 @@
 class ForumsController < ApplicationController
 
-	before_filter :admin_required, :except => [:index, :show]
+	before_filter :admin_required, :except => [:index, :show, :new, :create]
 
 	def new
+		@forum = Forum.new
+	end
+
+	def create
+		@forum = Forum.new(params[:forum])
+		if @forum.save
+			flash[:success] = "Forum created successfully!"
+			redirect_to forum_path(@forum)
+		else
+			render 'new'
+		end
 	end
 
 	def index
